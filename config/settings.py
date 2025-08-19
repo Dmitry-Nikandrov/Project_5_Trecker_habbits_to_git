c09060756900d40c88ac5a14a508c5e6cbb867a4
Dmitry <dnikandrov@yandex.ru>
2025-08-19 21:47:35 +0300

    35.2 ci/cd и github actions

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_I18N = True
 
 STATIC_URL = "static/"
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "media/"
 
